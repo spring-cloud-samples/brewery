2a91deb84d216f49def0f347d74144f06122e507
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2015-12-09 21:27:49 +0100

    Changed retry to be executed from a script

--- acceptance-tests/runGradle.sh
+++ acceptance-tests/runGradle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # TODO: Retry takes place due to issues with SocketTimeouts on first try (will have to investigate that)
-docker-compose run acceptance-tests ./runGradle.sh
+./gradlew test "$@" --stacktrace --info --no-daemon || ./gradlew test "$@" --stacktrace --info --no-daemon
