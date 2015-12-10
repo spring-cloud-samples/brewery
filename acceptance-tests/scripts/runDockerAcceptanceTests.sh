3b781cb10d7123bdc3958dd367dc5e478be1df96
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2015-12-10 15:51:37 +0100

    Polished the bash script (kudos to @wybczu)

--- acceptance-tests/scripts/runDockerAcceptanceTests.sh
+++ acceptance-tests/scripts/runDockerAcceptanceTests.sh
@@ -1,54 +1,56 @@
 #!/bin/bash
 
-set -e
+set -o errexit
+
+REPO_URL="${REPO_URL:-https://github.com/spring-cloud-samples/brewery.git}"
+REPO_BRANCH="${REPO_BRANCH:-master}"
+REPO_LOCAL="${REPO_LOCAL:-brewery}"
 
 # Parse the script arguments
-for i in "$@"
-do
-
-TEST_OPTS=""
-case $i in
-    -t=*|--test=*)
-    WHAT_TO_TEST="${i#*=}"
-    shift
-    ;;
-    -to=*|--testopts=*)
-    TEST_OPTS="${i#*=}"
-    shift
-    ;;
-    -v=*|--version=*)
-    VERSION="${i#*=}"
-    shift
-    ;;
-    *)
-    ;;
-esac
+for i in "$@"; do
+    TEST_OPTS=""
+    case $i in
+        -t=*|--test=*)
+            WHAT_TO_TEST="${i#*=}"
+            shift
+            ;;
+        -to=*|--testopts=*)
+            TEST_OPTS="${i#*=}"
+            shift
+            ;;
+        -v=*|--version=*)
+            VERSION="${i#*=}"
+            shift
+            ;;
+        *)
+            ;;
+    esac
 done
 
-echo -e "\n\nRunning tests with the following parameters"
-echo -e "\nWHAT_TO_TEST=$WHAT_TO_TEST"
-echo -e "\nTEST_OPTS=$TEST_OPTS"
-echo -e "\nVERSION=$VERSION"
-echo -e "\n\n"
+cat <<EOF
+
+Running tests with the following parameters
+
+WHAT_TO_TEST=${WHAT_TO_TEST}
+TEST_OPTS=${TEST_OPTS}
+VERSION=${VERSION}
+
+EOF
 
 # Clone or update the brewery repository
-REPOSRC=https://github.com/spring-cloud-samples/brewery.git
-LOCALREPO=brewery
-
-LOCALREPO_VC_DIR=$LOCALREPO/.git
-
-if [ ! -d $LOCALREPO_VC_DIR ]
-then
-    git clone $REPOSRC $LOCALREPO
-    cd $LOCALREPO
+if [[ ! -d "${REPO_LOCAL}/.git" ]]; then
+    git clone "${REPO_URL}" "${REPO_LOCAL}"
+    cd "${REPO_LOCAL}"
 else
-    cd $LOCALREPO
+    cd "${REPO_LOCAL}"
     git reset --hard
-    git pull $REPOSRC master
+    git pull "${REPO_URL}" "${REPO_BRANCH}"
 fi
 
 # Update the desired library version
 echo "$WHAT_TO_TEST=$VERSION" >> gradle.properties
+
+echo "\n\nUsing the following gradle.properties"
 cat gradle.properties
 
 # Build and run docker images
@@ -62,31 +64,27 @@ docker-compose up -d
 url="http://127.0.0.1"
 waitTime=5
 retries=48
-totalWaitingTime=240
+totalWaitingTime=$(( waitTime * retries ))
 n=0
 success=false
 
-echo "Waiting for the apps to boot for [$totalWaitingTime] seconds"
-until [ $n -ge $retries ]
-do
-  echo "Pinging applications if they're alive..."
-  curl $url:9991/health &&
-  curl $url:9992/health &&
-  curl $url:9993/health &&
-  curl $url:9994/health && success=true && break
-  n=$[$n+1]
-  echo "Failed... will try again in [$waitTime] seconds"
-  sleep $waitTime
+echo "\n\nWaiting for the apps to boot for [$totalWaitingTime] seconds"
+until [ $n -ge $retries ]; do
+    echo "Pinging applications if they're alive..."
+    curl $url:9991/health &&
+    curl $url:9992/health &&
+    curl $url:9993/health &&
+    curl $url:9994/health && success=true && break
+    n=$(( n+1 ))
+    echo "Failed... will try again in [$waitTime] seconds"
+    sleep $waitTime
 done
 
 # Run acceptance tests
 if [ "$success" = true ] ; then
-  echo -e "\n\nSuccessfully booted up all the apps. Proceeding with the acceptance tests"
-  cat runAcceptanceTests.sh
-  bash -e runAcceptanceTests.sh "-DWHAT_TO_TEST=$WHAT_TO_TEST"
+    echo -e "\n\nSuccessfully booted up all the apps. Proceeding with the acceptance tests"
+    bash -e runAcceptanceTests.sh "-DWHAT_TO_TEST=$WHAT_TO_TEST"
 else
-  echo -e "\n\nFailed to boot the apps."
-  exit 1
+    echo -e "\n\nFailed to boot the apps."
+    exit 1
 fi
-
-cd ..
