53cf3e43897a0f78c18a926a486ddd68db5999df
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-01-25 18:23:16 +0100

    Made all apps use a different port for rabbitmq

--- docker-compose-CONSUL.sh
+++ docker-compose-CONSUL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SYSTEM_PROPS="-DRABBIT_HOST=${HEALTH_HOST} -Dspring.zipkin.enabled=false -Dspring.profiles.active=consul -Dspring.cloud.consul.host=${HEALTH_HOST}"
+SYSTEM_PROPS="-DRABBIT_HOST=${HEALTH_HOST} -Dspring.rabbitmq.port=9672 -Dspring.zipkin.enabled=false -Dspring.profiles.active=consul -Dspring.cloud.consul.host=${HEALTH_HOST}"
 
 dockerComposeFile="docker-compose-${WHAT_TO_TEST}.yml"
 docker-compose -f $dockerComposeFile kill
@@ -14,7 +14,7 @@ echo -e "\n\nBooting up Discovery"
 docker-compose -f $dockerComposeFile up -d discovery
 
 READY_FOR_TESTS="no"
-PORT_TO_CHECK=5672
+PORT_TO_CHECK=9672
 echo "Waiting for RabbitMQ to boot for [$(( WAIT_TIME * RETRIES ))] seconds"
 netcat_port $PORT_TO_CHECK && READY_FOR_TESTS="yes"
 
