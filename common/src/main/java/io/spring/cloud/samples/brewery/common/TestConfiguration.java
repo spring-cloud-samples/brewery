c8b1b3665f8334af1f07519c1032743cb14d80d0
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-02-02 13:27:54 +0100

    Fixed wrong classpath, added debug for s-cloud
    * Sleuth Stream was on classpath and was overriding the Sleuth's ZikpinSpan reporter

--- common/src/main/java/io/spring/cloud/samples/brewery/common/TestConfiguration.java
+++ common/src/main/java/io/spring/cloud/samples/brewery/common/TestConfiguration.java
@@ -1,6 +1,8 @@
 package io.spring.cloud.samples.brewery.common;
 
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,11 @@ public class TestConfiguration {
 	@Bean
 	public FilterRegistrationBean correlationIdFilter() {
 		return new FilterRegistrationBean(new TestConfigurationSettingFilter());
+	}
+
+	@Bean
+	Sampler defaultSampler() {
+		return new AlwaysSampler();
 	}
 
 }
