bebc86de91412df4b46591901bed6ae417aa90a3
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-02-15 13:22:37 +0100

    [#10] Added Span reporter that stores and prints spans
    - each span when using Zipkin is stored in memory
    - it's printed to the console
    - you can access it via /spans/{traceId}
    
    Fixes #10

---
+++ common-zipkin/src/main/java/io/spring/cloud/samples/brewery/common/StoringZipkinSpanReporter.java
@@ -0,0 +1,54 @@
+package io.spring.cloud.samples.brewery.common;
+
+import java.util.Collection;
+
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.metric.SpanReporterService;
+import org.springframework.cloud.sleuth.zipkin.HttpZipkinSpanReporter;
+import org.springframework.cloud.sleuth.zipkin.ZipkinProperties;
+import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import zipkin.Span;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@Component
+@RestController
+@Slf4j
+public class StoringZipkinSpanReporter implements ZipkinSpanReporter {
+
+	private final Multimap<Long, Span> sentSpans = Multimaps.synchronizedListMultimap(
+			LinkedListMultimap.create());
+	private final HttpZipkinSpanReporter delegate;
+
+	@Autowired
+	public StoringZipkinSpanReporter(SpanReporterService spanReporterService, ZipkinProperties zipkin) {
+		delegate = new HttpZipkinSpanReporter(zipkin.getBaseUrl(), zipkin.getFlushInterval(),
+				spanReporterService);
+	}
+
+	@RequestMapping("/spans/{traceId}")
+	public ResponseEntity<Collection<Span>> spans(@PathVariable String traceId) {
+		Collection<Span> spansForTrace = sentSpans
+				.get(org.springframework.cloud.sleuth.Span.fromHex(traceId));
+		return ResponseEntity.ok(spansForTrace);
+	}
+
+	@Override
+	public void report(Span span) {
+		sentSpans.put(span.traceId, span);
+		log.info("Sending span [" + span.toString() + "] to Zipkin");
+		delegate.report(span);
+	}
+}
