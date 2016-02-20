609e9bc0c132a1b3328c59cfb14c702373cb3917
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-02-20 08:52:37 +0100

    Removed wrong classpath for stream

--- zipkin-server/src/main/java/io/spring/cloud/samples/brewery/zipkin/Application.java
+++ zipkin-server/src/main/java/io/spring/cloud/samples/brewery/zipkin/Application.java
@@ -1,4 +1,4 @@
-package io.spring.cloud.samples.brewery.maturing;
+package io.spring.cloud.samples.brewery.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
