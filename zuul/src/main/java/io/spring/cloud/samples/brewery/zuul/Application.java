46596ddd6e0b984fee2c2a8d1008721a2fc919e9
Spencer Gibb <spencer@gibb.us>
2017-10-20 12:20:49 -0400

    adds @EnableDiscoveryClient

--- zuul/src/main/java/io/spring/cloud/samples/brewery/zuul/Application.java
+++ zuul/src/main/java/io/spring/cloud/samples/brewery/zuul/Application.java
@@ -2,6 +2,7 @@ package io.spring.cloud.samples.brewery.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.sleuth.util.ExceptionUtils;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 @Configuration
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         ExceptionUtils.setFail(true);
