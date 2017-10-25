830ec51fab2cad50eb10cc025de767903414df7c
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-10-25 13:47:14 +0200

    reverting

--- zuul/src/main/java/io/spring/cloud/samples/brewery/zuul/Application.java
+++ zuul/src/main/java/io/spring/cloud/samples/brewery/zuul/Application.java
@@ -1,18 +1,11 @@
 package io.spring.cloud.samples.brewery.zuul;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.sleuth.util.ExceptionUtils;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @Configuration
