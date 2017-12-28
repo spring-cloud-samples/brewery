dba1f4fe4a0630c6f04b53de0293a0c653136222
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-12-28 12:08:55 +0100

    Hopefully fixed the build

--- runAcceptanceTests.sh
+++ runAcceptanceTests.sh
@@ -274,7 +274,7 @@ GLOBAL:
 -k  |--kafka - uses Kafka instead of RabbitMQ
 -d  |--skipdeployment - should skip deployment of apps? Defaults to "no"
 -a  |--deployonlyapps - should deploy only the brewery business apps instead of the infra too? Defaults to "no"
--b  |--bootversion - Which version of Boot should be used? Defaults to 1.4.4.RELEASE for the plugin and to boot version used by libs
+-b  |--bootversion - Which version of Boot should be used? Defaults to 1.5.9.RELEASE for the plugin and to boot version used by libs
 -cli|--cliversion - which version of Spring Cloud CLI should be used (it's used to start Kafka)? Defaults to 1.2.3.RELEASE
 -ve |--verbose - Will print all library versions
 
