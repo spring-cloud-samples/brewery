2dfd3a9fb9b28971eab40817f8664d30b76c5b22
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-09-10 13:53:42 +0200

    Can't get any worse than this

--- ingredients/src/main/java/io/spring/cloud/samples/brewery/Application.java
+++ ingredients/src/main/java/io/spring/cloud/samples/brewery/Application.java
@@ -3,9 +3,13 @@ package io.spring.cloud.samples.brewery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+import io.spring.cloud.samples.brewery.common.events.EventSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableBinding(EventSource.class)
 public class Application {
 
     public static void main(String[] args) {
