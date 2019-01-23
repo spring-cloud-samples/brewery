56775b11ef4b45b43d85618c0d1fc715c15fac89
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-01-23 17:41:31 +0100

    Warm up the env

--- acceptance-tests/src/test/groovy/io/spring/cloud/samples/brewery/acceptance/SleuthBreweryAcceptanceTests.java
+++ acceptance-tests/src/test/groovy/io/spring/cloud/samples/brewery/acceptance/SleuthBreweryAcceptanceTests.java
@@ -20,9 +20,12 @@ public class SleuthBreweryAcceptanceTests extends AbstractBreweryAcceptance {
 	@Test
 	public void should_successfully_pass_Trace_Id_via_rest_template() {
 		// setup:
-		warm_up_the_environment(this::should_successfully_pass_Trace_Id_via_feign);
+		warm_up_the_environment(() -> check_brewery(CommunicationType.REST_TEMPLATE));
 		// given:
-		CommunicationType communicationType = CommunicationType.REST_TEMPLATE;
+		check_brewery(CommunicationType.REST_TEMPLATE);
+	}
+
+	private void check_brewery(CommunicationType communicationType) {
 		String referenceProcessId = SpanUtil.idToHex(new Random().nextLong());
 		RequestEntity requestEntity = an_order_for_all_ingredients_with_process_id(referenceProcessId, communicationType);
 		// when:
@@ -41,22 +44,9 @@ public class SleuthBreweryAcceptanceTests extends AbstractBreweryAcceptance {
 	@Test
 	public void should_successfully_pass_Trace_Id_via_feign() {
 		// setup:
-		warm_up_the_environment(this::should_successfully_pass_Trace_Id_via_rest_template);
+		warm_up_the_environment(() -> check_brewery(CommunicationType.FEIGN));
 		// given:
-		CommunicationType communicationType = CommunicationType.FEIGN;
-		String referenceProcessId = SpanUtil.idToHex(new Random().nextLong());
-		RequestEntity requestEntity = an_order_for_all_ingredients_with_process_id(referenceProcessId, communicationType);
-		// when:
-		presenting_service_has_been_called(requestEntity);
-		// and:
-		requestEntity = an_order_for_all_ingredients_with_process_id(referenceProcessId, communicationType);
-		presenting_service_has_been_called(requestEntity);
-		// then:
-		beer_has_been_brewed_for_process_id(referenceProcessId);
-		// and:
-		entry_for_trace_id_is_present_in_Zipkin(referenceProcessId);
-		// and:
-		dependency_graph_is_correct();
+		check_brewery(CommunicationType.FEIGN);
 	}
 
 }
