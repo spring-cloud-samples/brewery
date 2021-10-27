11799c9bacc69a5282bb6ae5768f19ab7664663c
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-10-27 13:31:38 +0200

    Fix FeignClient setup.

--- presenting/src/main/java/io/spring/cloud/samples/brewery/presenting/present/BrewingServiceClient.java
+++ presenting/src/main/java/io/spring/cloud/samples/brewery/presenting/present/BrewingServiceClient.java
@@ -9,13 +9,11 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(Collaborators.BREWING)
-@RequestMapping(value = "/ingredients",
-		consumes = Versions.BREWING_CONTENT_TYPE_V1,
-		produces = MediaType.APPLICATION_JSON_VALUE)
+@FeignClient(name = Collaborators.BREWING, path = "/ingredients")
 public interface BrewingServiceClient {
-	@RequestMapping(method = RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST, consumes = Versions.BREWING_CONTENT_TYPE_V1,
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	String getIngredients(String body,
-						  @RequestHeader("PROCESS-ID") String processId,
-						  @RequestHeader("TEST-COMMUNICATION-TYPE") String testCommunicationType);
+			@RequestHeader("PROCESS-ID") String processId,
+			@RequestHeader("TEST-COMMUNICATION-TYPE") String testCommunicationType);
 }
