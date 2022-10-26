babae6117da0785e311214a4fa98e7779eae1a3a
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-10-27 00:44:08 +0200

    Added workaround for otel & remote fields problem

--- common/src/main/java/io/spring/cloud/samples/brewery/common/TestConfiguration.java
+++ common/src/main/java/io/spring/cloud/samples/brewery/common/TestConfiguration.java
@@ -1,5 +1,19 @@
 package io.spring.cloud.samples.brewery.common;
 
+import java.util.Collections;
+import java.util.List;
+
+import io.micrometer.tracing.otel.bridge.OtelBaggageManager;
+import io.micrometer.tracing.otel.bridge.OtelCurrentTraceContext;
+import io.micrometer.tracing.otel.propagation.BaggageTextMapPropagator;
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+
+import org.springframework.boot.actuate.autoconfigure.tracing.TracingProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.IntegrationComponentScan;
@@ -8,5 +22,23 @@ import org.springframework.integration.annotation.IntegrationComponentScan;
 @ComponentScan
 @IntegrationComponentScan
 public class TestConfiguration {
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnClass(OtelCurrentTraceContext.class)
+	static class OtelConfig {
+
+		private TextMapPropagator otelRemoteFieldsBaggageTextMapPropagator(TracingProperties tracingProperties, OtelCurrentTraceContext otelCurrentTraceContext) {
+			List<String> remoteFields = tracingProperties.getBaggage().getRemoteFields();
+			return new BaggageTextMapPropagator(remoteFields,
+				new OtelBaggageManager(otelCurrentTraceContext, remoteFields, Collections.emptyList()));
+		}
+
+		// TODO: Remove me after Boot config gets fixed via https://github.com/spring-projects/spring-boot/pull/32898
+		@Bean
+		ContextPropagators w3cContextPropagators(TracingProperties tracingProperties, OtelCurrentTraceContext otelCurrentTraceContext) {
+			return ContextPropagators.create(TextMapPropagator.composite(W3CTraceContextPropagator.getInstance(),
+				W3CBaggagePropagator.getInstance(), otelRemoteFieldsBaggageTextMapPropagator(tracingProperties, otelCurrentTraceContext)));
+		}
+	}
 
 }
