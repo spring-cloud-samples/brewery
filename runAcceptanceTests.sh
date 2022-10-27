6db0d48266cd6c2c4b95bc2ec7887b51c869a7d2
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-10-27 13:10:56 +0200

    Fixing acceptance tests

--- runAcceptanceTests.sh
+++ runAcceptanceTests.sh
@@ -181,6 +181,7 @@ GLOBAL:
 -v  |--version - which version of BOM do you want to use? Defaults to current release train snapshot
 -h  |--healthhost - what is your health host? where is docker? defaults to localhost
 -l  |--numberoflines - how many lines of logs of your app do you want to print? Defaults to 1000
+-r  |--reset - do you want to reset the git repo of brewery? Defaults to "no"
 -ke |--killattheend - should kill all the running apps at the end of execution? Defaults to "no"
 -n  |--killnow - should not run all the logic but only kill the running apps? Defaults to "no"
 -x  |--skiptests - should skip running of e2e tests? Defaults to "no"
@@ -249,6 +250,9 @@ while [[ $# > 0 ]]; do
 	-l | --numberoflines)
 		NUMBER_OF_LINES_TO_LOG="$2"
 		shift # past argument
+		;;
+	-r | --reset)
+		RESET="yes"
 		;;
 	-ke | --killattheend)
 		KILL_AT_THE_END="yes"
@@ -388,6 +392,10 @@ if [[ ! -e "${REPO_LOCAL}/.git" ]]; then
 	git checkout "${REPO_BRANCH}"
 else
 	cd "${REPO_LOCAL}"
+	if [[ ${RESET} ]]; then
+		git reset --hard
+		git pull "${REPO_URL}" "${REPO_BRANCH}"
+	fi
 fi
 CURRENT_DIR=$(pwd)
 
